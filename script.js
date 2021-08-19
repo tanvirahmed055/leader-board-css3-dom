071b841a0b30d2e29d8a9167698d01e8fb4b77a8
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-08-20 01:20:55 +0600

    v2

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ topPlayersHeader.style.color = "blue";
 const topBlogsHeader = document.getElementById("top-blogs-heading");
 
 topBlogsHeader.style.color = "blue";
+
+
+//২. জাভাস্ক্রিপ্ট দিয়ে player ক্লাস এর যতগুলা div আছে সবগুলাকে যেকোন একটা ব্যাকগ্রাউন্ড কালার দাও। তোমার ইচ্ছামতো যেকোন একটা ব্যাকগ্রাউন্ড কালার দিতে হবে। তবে কালারটা hex কালার না দিয়ে। rgba কালার দাও। যদি rgba বুঝতে না পারো। তাহলে গুগলে সার্চ দিয়ে একটা rgba কালার বের করে তারপর সেই কালার দাও।  
+
+
+const players = document.getElementsByClassName("player");
+for (const player of players) {
+    player.style.backgroundColor = "rgba(192,192,192,0.3)";
+}

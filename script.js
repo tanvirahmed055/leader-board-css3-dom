d94df7e869d0f8a2cb73cb16006abdd24c94b714
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-08-20 01:03:27 +0600

    changed element color with js dom

---
+++ script.js
@@ -0,0 +1,11 @@
+//১. Top Players2022 নাম একটা লেখা আছে। অথবা Top Blogs লেখা আছে। এই দুইটাকে দুইটা ID দাও। HTML চেইঞ্জ করে ID সেট করো। তারপর জাভাস্ক্রিপ্ট দিয়েই সেই এলিমেন্ট খুঁজে বের করে তার টেক্সট এর কালার চেইঞ্জ করে ফেলো।
+
+const topPlayersHeader = document.getElementById("top-players-heading");
+
+topPlayersHeader.style.color = "blue";
+
+
+
+const topBlogsHeader = document.getElementById("top-blogs-heading");
+
+topBlogsHeader.style.color = "blue";
